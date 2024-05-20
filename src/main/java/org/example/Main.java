5db4a240f01b011036a286d141595681d8490be8
yosemm <140467686+yosemm@users.noreply.github.com>
2024-05-20 01:36:09 -0600

    Create main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        RecommendationSystem recommendationSystem = new RecommendationSystem();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the artist you want to search for:");
+        String artistName = scanner.nextLine();
+
+        List<Artist> similarArtists = recommendationSystem.getSimilarArtists(artistName);
+
+        for (Artist artist : similarArtists) {
+            System.out.println("Similar artist to " + artistName + ": " + artist.getName());
+            System.out.println("Mood: " + artist.getMood());
+            System.out.println("Genre: " + artist.getGenre());
+        }
+    }
+}
